c95c58ff4cf7f2b85e6bbe82cdd65c4c60502524
Unbinilium <15633984+Unbinilium@users.noreply.github.com>
2022-09-05 21:17:07 +0800

    implement tcp session

---
+++ src/chassis_bridge/include/chassis_bridge/networking.hpp
@@ -0,0 +1,143 @@
+#pragma once
+
+#include <memory>
+#include <exception>
+#include <utility>
+#include <iostream>
+
+#include <asio.hpp>
+
+#include "chassis_bridge/protocol.hpp"
+#include "chassis_bridge/types.hpp"
+#include "chassis_bridge/container.hpp"
+
+namespace cb::networking {
+    namespace tcp {
+        class session : public std::enable_shared_from_this<session> {
+            using rx_deque_item = std::unique_ptr<cb::types::underlying::rx::frame>;
+            using tx_deque_item = std::unique_ptr<cb::types::underlying::tx::frame>;
+        public:
+            session(asio::ip::tcp::socket                    socket,
+                    std::size_t                              buffer_size,
+                    cb::container::ts::deque<rx_deque_item>* receive_deque_ptr,
+                    cb::container::ts::deque<tx_deque_item>* transmit_deque_ptr
+            ) : socket_(std::move(socket)),
+                receive_deque_ptr_(receive_deque_ptr),
+                transmit_deque_ptr_(transmit_deque_ptr) {
+                    std::cout << std::chrono::system_clock::now().time_since_epoch().count() 
+                              << "[tcp session] client connected from: " << socket_.remote_endpoint() << std::endl;
+            }
+
+            void start() {
+                do_write();
+                do_read();
+                do_write_heartbeat();
+            }
+
+        protected:
+            virtual void on_reading_header(asio::ip::tcp::socket&) {}
+            virtual void on_reading_body(asio::ip::tcp::socket&) {}
+            virtual void on_reading_finished(asio::ip::tcp::socket&) {
+                std::this_thread::yield();
+                do_read();
+            }
+            virtual void on_writing_finished(asio::ip::tcp::socket&) {
+                std::this_thread::yield();
+                transmit_deque_ptr_->wait();
+                do_write();
+            }
+            virtual void on_heartbeat_finished(asio::ip::tcp::socket&) {
+                static auto start{std::chrono::system_clock::now()};
+                auto end{std::chrono::system_clock::now()};
+                using namespace std::chrono_literals;
+                if (end - start > 10ms) {
+                    do_write_heartbeat();
+                }
+            }
+
+        private:
+            void do_read() {
+                auto self(shared_from_this());
+                auto header{std::make_unique<cb::types::underlying::head>()};
+
+                socket_.async_read_some(
+                    asio::buffer(reinterpret_cast<void*>(header.get()), sizeof(cb::types::underlying::head)),
+                    [this, self, header = std::move(header)](std::error_code ec, std::size_t byte) {
+                        auto rx_frame{std::make_unique<cb::types::underlying::rx::frame>()};
+                        if (ec) [[unlikely]] {
+                            std::cout << std::chrono::system_clock::now().time_since_epoch().count() 
+                                      << "[tcp session] read " << byte << " byte header failed: " << ec.message() << std::endl;
+                            socket_.close();
+                            std::cout << std::chrono::system_clock::now().time_since_epoch().count() 
+                                      << "[tcp session] session closed from: " << socket_.remote_endpoint() << std::endl;
+                        }
+                        if (*header == rx_frame->header) {
+                            std::cout << std::chrono::system_clock::now().time_since_epoch().count() 
+                                      << "[tcp session] read " << byte << " byte header: '" << *header << "'" << std::endl;
+                            on_reading_header(socket_);
+                            socket_.async_read_some(
+                                asio::buffer(reinterpret_cast<void*>(&rx_frame->body), sizeof(cb::types::underlying::rx::data)),
+                                [this, self, rx_frame = std::move(rx_frame)](std::error_code ec, std::size_t byte) {
+                                    if (!ec) {
+                                        std::cout << std::chrono::system_clock::now().time_since_epoch().count() 
+                                                  << "[tcp session] read " << byte << " byte body"  << std::endl;
+                                        on_reading_body(socket_);
+                                        receive_deque_ptr_->push_back(std::move(rx_frame));
+                                        on_reading_finished(socket_);
+                                    } else {
+                                        std::cout << std::chrono::system_clock::now().time_since_epoch().count() 
+                                                  << "[tcp session] read " << byte << " byte header failed: "  << ec.message() << std::endl;
+                                        socket_.close();
+                                        std::cout << std::chrono::system_clock::now().time_since_epoch().count() 
+                                                  << "[tcp session] session closed from: " << socket_.remote_endpoint() << std::endl;
+                                    }
+                            });
+                        }
+                });
+            }
+
+            void do_write() {
+                if (transmit_deque_ptr_->empty()) on_writing_finished(socket_);
+                auto self(shared_from_this());
+                asio::async_write(socket_,
+                    asio::buffer(reinterpret_cast<void*>(transmit_deque_ptr_->front().get()), sizeof(cb::types::underlying::tx::frame)), 
+                    [this, self](std::error_code ec, std::size_t byte) {
+                        if (ec) [[unlikely]] {
+                            std::cout << std::chrono::system_clock::now().time_since_epoch().count() 
+                                      << "[tcp session] write " << byte << " byte frame failed: "  << ec.message() << std::endl;
+                            socket_.close();
+                            std::cout << std::chrono::system_clock::now().time_since_epoch().count() 
+                                      << "[tcp session] session closed from: " << socket_.remote_endpoint() << std::endl;
+                        }
+                        std::cout << std::chrono::system_clock::now().time_since_epoch().count() 
+                                  << "[tcp session] write " << byte << " byte frame"  << std::endl;
+                        transmit_deque_ptr_->pop_front();
+                        on_writing_finished(socket_);
+                });
+            }
+
+            void do_write_heartbeat() {
+                auto self(shared_from_this());
+                static auto heartbeat{cb::types::underlying::tx::heartbeat()};
+                asio::async_write(socket_,
+                    asio::buffer(reinterpret_cast<void*>(&heartbeat), sizeof(cb::types::underlying::tx::heartbeat)), 
+                    [this, self](std::error_code ec, std::size_t byte) {
+                        if (ec) [[unlikely]] {
+                            std::cout << std::chrono::system_clock::now().time_since_epoch().count() 
+                                      << "[tcp session] write " << byte << " byte heartbeat failed: "  << ec.message() << std::endl;
+                            socket_.close();
+                            std::cout << std::chrono::system_clock::now().time_since_epoch().count() 
+                                      << "[tcp session] session closed from: " << socket_.remote_endpoint() << std::endl;
+                        }
+                        on_heartbeat_finished(socket_);
+                });
+            }
+
+            asio::ip::tcp::socket socket_;
+
+            cb::container::ts::deque<rx_deque_item>* receive_deque_ptr_;
+            cb::container::ts::deque<tx_deque_item>* transmit_deque_ptr_;
+        };
+
+    };
+};
