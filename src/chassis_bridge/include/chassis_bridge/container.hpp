30a17f35d45780b3bc549b204c02c549f88a0be7
Unbinilium <15633984+Unbinilium@users.noreply.github.com>
2022-09-06 10:51:51 +0800

    refine ts::deque type determine

--- src/chassis_bridge/include/chassis_bridge/container.hpp
+++ src/chassis_bridge/include/chassis_bridge/container.hpp
@@ -14,19 +14,19 @@ namespace cb::container {
             deque()  = default;
             ~deque() = default;
 
+            void push_back(const T& data) {
+                std::unique_lock lock(mutex_);
+                data_.push_back(data);
+                cv_.notify_one();
+            }
+
             void push_back(const T&& data) {
                 std::unique_lock lock(mutex_);
                 data_.push_back(std::forward<T>(data));
                 cv_.notify_one();
             }
 
-            void push_back(T&& data) {
-                std::unique_lock lock(mutex_);
-                data_.push_back(std::forward<T>(data));
-                cv_.notify_one();
-            }
-
-            auto front() const {
+            decltype(auto) front() const {
                 std::shared_lock lock(mutex_);
                 return data_.front();
             }
